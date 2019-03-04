623bf5cd2a519e7c6015408cc3ea2ce45a9089d1
Felix Fennell <felnne@bas.ac.uk>
2019-03-04 17:56:29 +0000

    Splitting app.py into app.py and manage.py
    This resolves the issue of having an application created (and ran) on importing the app module, whilst giving us somewhere we can run an application from for use with the Flask CLI (used locally and in CI).

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-import os
-import unittest
-
 from flask import Flask, jsonify, url_for
 from flask_reverse_proxy_fix.middleware import ReverseProxyPrefixFix
 
@@ -11,13 +8,6 @@ def create_app_with_middleware():
     # Configure and load Middleware
     app.config['REVERSE_PROXY_PATH'] = '/test'
     ReverseProxyPrefixFix(app)
-
-    # Support running integration tests
-    @app.cli.command()
-    def test():
-        """Run integration tests."""
-        tests = unittest.TestLoader().discover(os.path.join(os.path.dirname(__file__), 'tests'))
-        unittest.TextTestRunner(verbosity=2).run(tests)
 
     @app.route("/sample")
     def sample():
@@ -35,13 +25,6 @@ def create_app_with_middleware():
 def create_app_without_middleware():
     app = Flask(__name__)
 
-    # Support running integration tests
-    @app.cli.command()
-    def test():
-        """Run integration tests."""
-        tests = unittest.TestLoader().discover(os.path.join(os.path.dirname(__file__), 'tests'))
-        unittest.TextTestRunner(verbosity=2).run(tests)
-
     @app.route("/sample")
     def sample():
         payload = {
@@ -53,15 +36,3 @@ def create_app_without_middleware():
         return jsonify(payload)
 
     return app
-
-
-if __name__ == "__main__":
-    test_app = create_app_with_middleware()
-
-    # Support PyCharm debugging
-    if 'PYCHARM_HOSTED' in os.environ:
-        # Exempting Bandit security issue (binding to all network interfaces)
-        #
-        # All interfaces option used because the network available within the container can vary across providers
-        # This is only used when debugging with PyCharm. A standalone web server is used in production.
-        test_app.run(host='0.0.0.0', port=9000, debug=True, use_debugger=False, use_reloader=False)  # nosec
